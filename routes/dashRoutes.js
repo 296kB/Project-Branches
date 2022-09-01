ebfad93e55b6afafcfe8391a42ce3227d7fa7a18
JosephPagan <99441821+JosephPagan@users.noreply.github.com>
2022-09-01 13:12:50 -0400

    early routes to include

--- routes/dashRoutes.js
+++ routes/dashRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const homeController = require('../controllers/home')
+const dashController = require('../controllers/home')
 const authMiddleware = require('../middleware/auth')
 
-router.get('/', homeController.getIndex)
-
+router.get('/', dashController.getDash)
