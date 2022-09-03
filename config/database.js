d27f9a136f29db77ea43a88196c95e6d792d7319
itsdaijoebu <joseph@josephchan.me>
2022-09-03 09:05:23 +0300

    update dependencies and syntax, bug fixes
    installed bcrypt as a dependency, updated syntax from mongoose 5 to 6, fixed other misc bugs that were preventing startup

--- config/database.js
+++ config/database.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
       dbName: 'branch'
     })
 
--- routes/dashRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const dashController = require('../controllers/home')
-const authMiddleware = require('../middleware/auth')
-
-router.get('/', dashController.getDash)
-router.post('/postproject', dashController.postProject)
-router.delete('/delete', dashController.deleteProject)
-
