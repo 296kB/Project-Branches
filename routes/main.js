841cd8ad91303f8c3417c0b3161f9e6eac962bc2
krogo81611 <krogo81611@gmail.com>
2022-09-03 11:22:47 -0600

    Commit changes for sign up page

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/home')
 const authMiddleware = require('../middleware/auth')
 
 router.get('/', homeController.getIndex)
+router.get('/signup', homeController.getSignup)
 
 
 module.exports = router
