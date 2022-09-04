73b7c74e40a8fcfaad5622925de88da7e3312aa5
JosephPagan <99441821+JosephPagan@users.noreply.github.com>
2022-09-04 12:51:32 -0400

    added projectDetail routes and updated project view

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const dashController = require('../controllers/dashboard')
+const projController = require('../controllers/project')
 const authMiddleware = require('../middleware/auth')
 
-// router.get('/', dashController.getDash)
-// router.post('/postProject', dashController.postProject)
-// router.delete('/delete', dashController.deleteProject)
+router.get('/', dashController.getProjects)
+router.post('/postProject', projController.createProject)
+router.get('/project', projController.getProject)
 
 module.exports = router
