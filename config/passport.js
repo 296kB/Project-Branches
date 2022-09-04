4070b20a9dacee9b9b5c772fea83a28ec0bfc9d4
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2022-09-04 16:51:25 -0400

    fixed login redirect

--- config/passport.js
+++ config/passport.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const User = require('../models/User')
 
 module.exports = function (passport) {
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+
+  passport.use(new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+
       if (err) { return done(err) }
       if (!user) {
         return done(null, false, { msg: `Email ${email} not found.` })
--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ const User = require('../models/User')
   
     if (validationErrors.length) {
       req.flash('errors', validationErrors)
-      return res.redirect('/login')
+      return res.redirect('/')
     }
     req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
@@ -26,7 +26,7 @@ const User = require('../models/User')
       if (err) { return next(err) }
       if (!user) {
         req.flash('errors', info)
-        return res.redirect('/login')
+        return res.redirect('/')
       }
       req.logIn(user, (err) => {
         if (err) { return next(err) }
