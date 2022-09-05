c9fd0a5e684949608245a9a2e4933ef1a5c739fb
JosephPagan <99441821+JosephPagan@users.noreply.github.com>
2022-09-05 13:50:55 -0400

    individual project view renders and delete project function works

--- routes/projectDetail.js
+++ routes/projectDetail.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const projController = require('../controllers/project')
 
+// router.get('/project', projController.getProject)
 router.post('/markComplete', projController.markComplete)
 router.post('/markIncomplete', projController.markIncomplete)
 router.delete('/projectDelete', projController.deleteProject)
